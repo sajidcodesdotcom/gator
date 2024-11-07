7eb9f58531b2818e6fc095623f2230156c286c19
sajidcodess <muhammadsajidkhan616@gmail.com>
2024-11-07 20:10:27 +0500

    integrate postgresql via sqlca and finish register handler

--- handlers.go
+++ handlers.go
@@ -1,15 +1,58 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"time"
 
-func loginHandler (state *state, cmd command) error {
-  if len(cmd.Args) != 1 {
-    return fmt.Errorf("usage %s <name>", cmd.Name)
+	"github.com/google/uuid"
+	"github.com/sajidcodess/gator/internal/database"
+)
+
+func loginHandler(state *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage %s <name>", cmd.Name)
+	}
+	username := cmd.Args[0]
+  dbUser, err := state.db.GetUser(context.Background(), username)
+  if err != nil && dbUser.Name != username  {
+    return fmt.Errorf("User not found, please register before loging in: %s", err)
   }
-  username := cmd.Args[0]
-  if err := state.cfg.SetUser(username); err != nil {
-    return fmt.Errorf("the user %s is not found: %w", username, err)
+  
+	if err := state.cfg.SetUser(username); err != nil {
+		return fmt.Errorf("the user %s is not found: %w", username, err)
+	}
+	fmt.Println("The user has been set")
+	return nil
+}
+
+func registerHandler(state *state, cmd command) error {
+	if len(cmd.Args) != 1 {
+		return fmt.Errorf("usage %s <name>", cmd.Name)
+	}
+	username := cmd.Args[0]
+	params := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      username,
+	}
+
+  dbUser, err := state.db.GetUser(context.Background(), username)
+  if err != nil && dbUser.Name == username {
+    return fmt.Errorf("User already exist: %s", err)
   }
-  fmt.Println("The user has been set")
-  return nil
+
+	newUser, err := state.db.CreateUser(context.Background(), params)
+	if err != nil {
+		return err
+	}
+
+	fmt.Println("the user is created")
+
+	if err := state.cfg.SetUser(newUser.Name); err != nil {
+		return fmt.Errorf("the user %s is not found: %w", username, err)
+	}
+
+	return nil
 }
--- main.go
+++ main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
 	"os"
 
+	_ "github.com/lib/pq"
 	"github.com/sajidcodess/gator/internal/config"
+	"github.com/sajidcodess/gator/internal/database"
 )
 
 type state struct {
 	cfg *config.Config
+	db  *database.Queries
 }
 
 func main() {
@@ -18,17 +22,25 @@ func main() {
 		log.Fatalf("Error reading the config: %v", err)
 	}
 
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+    log.Fatalf("Error while opening DB connection: ", err)
+	}
+  dbQueries := database.New(db)
+
 	programState := &state{
 		cfg: &cfg,
+    db: dbQueries,
 	}
 
 	cmds := commands{
 		registerCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", loginHandler)
+  cmds.register("register", registerHandler)
 
 	if len(os.Args) < 2 {
-    os.Exit(1)
+		os.Exit(1)
 		fmt.Println("Usage: cli <command> [args...]")
 		return
 	}
